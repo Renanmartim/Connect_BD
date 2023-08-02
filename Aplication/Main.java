5141390954a8d16318e6637e03fa8c2b2105232d
Renan <renanmartim850@gmail.com>
2023-08-01 22:20:44 -0300

    Add connect and acess

--- Aplication/Main.java
+++ Aplication/Main.java
@@ -3,12 +3,29 @@ package Aplication;
 import Src.DBConnect;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection conn = DBConnect.getConnection();
-        DBConnect.closeConnection();
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DBConnect.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from car");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("id") + " , " + rs.getDate("entry_time"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
